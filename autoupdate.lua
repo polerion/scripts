9deb0b4162808cf49bfa2429210dd20fd5001dbf
polerion <105965514+polerion@users.noreply.github.com>
2022-05-21 11:17:58 +0300

    Update autoupdate.lua

--- autoupdate.lua
+++ autoupdate.lua
@@ -1,8 +1,8 @@
-script_name('Autoupdate script') -- íàçâàíèå ñêðèïòà
-script_author('FORMYS') -- àâòîð ñêðèïòà
-script_description('Autoupdate') -- îïèñàíèå ñêðèïòà
+script_name('Autoupdate script') -- Г­Г Г§ГўГ Г­ГЁГҐ Г±ГЄГ°ГЁГЇГІГ 
+script_author('FORMYS') -- Г ГўГІГ®Г° Г±ГЄГ°ГЁГЇГІГ 
+script_description('Autoupdate') -- Г®ГЇГЁГ±Г Г­ГЁГҐ Г±ГЄГ°ГЁГЇГІГ 
 
-require "lib.moonloader" -- ïîäêëþ÷åíèå áèáëèîòåêè
+require "lib.moonloader" -- ГЇГ®Г¤ГЄГ«ГѕГ·ГҐГ­ГЁГҐ ГЎГЁГЎГ«ГЁГ®ГІГҐГЄГЁ
 local dlstatus = require('moonloader').download_status
 local inicfg = require 'inicfg'
 local keys = require "vkeys"
@@ -17,7 +17,7 @@ local script_vers = 2
 local script_vers_text = "1.01"
 
 local script_path = thisScript().path
-local script_url = ''
+local script_url = 'https://raw.githubusercontent.com/polerion/scripts/main/autoupdate.lua'
 
 local update_path = getWorkingDirectory() .. "update.ini"
 local update_url = 'https://raw.githubusercontent.com/polerion/scripts/main/update.ini'
@@ -28,27 +28,27 @@ local update_url = 'https://raw.githubusercontent.com/polerion/scripts/main/upda
 function main()
 	if not isSampLoaded() or not isSampfuncsLoaded() then return end
     while not isSampAvailable() do wait(100) end
-    
+
     sampRegisterChatCommand("update", cmd_update)
 
     downloadUrlToFile(update_url, update_path, function(id, status)
         if status == dlstatus.STATUS_ENDDOWNLOADDATA then
             updateIni = inicfg.load (nil, update_path)
             if tonumber(updateIni.info.vers) > script_vers then
-                sampAddChatMessage('Åñòü îáíîâëåíèå. Âåðñèÿ: ' .. updateIni.info.vers_text, -1) 
+                sampAddChatMessage('Привет. Скрипт может быть обновлён. Новая версия:' .. updateIni.info.vers_text, -1)
                 update_state = true
             end
             os.remove(update_path)
         end
 
     end)
-    
+
 	while true do
         wait(0)
         if update_state then
             downloadUrlToFile(script_url, script_path, function(id, status)
                 if status == dlstatus.STATUS_ENDDOWNLOADDATA then
-                    sampAddChatMessage('Ñêðèïò îáíîâë¸í', -1)
+                    sampAddChatMessage('Скрипт обновлён', -1)
 
                 end
             end)
@@ -57,5 +57,5 @@ function main()
 end
 
 function cmd_update(arg)
-    sampShowDialog(1000, "Àâòîîáíîâëåíèå", "Ýòî óðîê ïî àâòîîáíîâëåíèþ", "Çàêðûòü", "", 0)
+    sampShowDialog(1000, "Автообновление", "Возможность автообновления", "Новая версия: ".. script_vers_text, "", 0)
 end
